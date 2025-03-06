fe5e5408fd950b0331998ffad132cfcc4c08af0f
Robin Højland Hansen <202409877@post.au.dk>
2025-03-06 14:37:31 +0100

    further setup and DB class with functions

---
+++ db_handler.py
@@ -0,0 +1,71 @@
+""" Implementing helper functions and classes related to databases"""
+import sqlite3
+
+class MessurementsDB:
+    """ Handles the messurements database, so it can connect and do queries and insertions """
+    db_conn = None
+    db_name = None
+    db_table_name = None
+
+    def __init__(self, db_name: str = "Measurements", db_table_name: str = "measurements"):
+        """ Initializes the database and its variables. Creates or connects to the database"""
+        self.db_name = db_name
+        self.db_table_name = db_table_name
+        self.db_conn = sqlite3.connect(self.db_name + ".db")
+        c = self.db_conn.cursor()
+
+        # AUTOINCREMENT increments id by one every time a new touple is created,
+        # if is is not declared in INSERT
+        # measurement is in JSON format
+        c.execute(
+            f"""
+            CREATE TABLE IF NOT EXIST {self.db_table_name}
+                (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                CO2 INTEGER
+                TVOC INTEGER
+                time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+                )
+            """
+        )
+        self.commit()
+
+    def commit(self):
+        """ Commits changes to the database"""
+        self.db_conn.commit()
+
+    def close(self):
+        """ Closes the connection to the databse """
+        self.db_conn.close()
+
+    def add_measurement(self, CO2, TVOC): #skal jeg seriøst skrive det med småt for pylint. co2 gør ondt at kigge på...
+        """ Adds a measurement to the database """
+        c = self.db_conn.cursor()
+        c.execute(f"""INSERT INTO {self.db_table_name} (CO2, TVOC)
+                  VALUES (?, ?)""",(CO2, TVOC))
+        self.commit()
+
+    def get_measurements(self):
+        """ Retrieves all measurements from the database """
+        c = self.db_conn.cursor()
+        c.execute(f"SELECT * from {self.db_table_name} ORDER BY time DESC")
+
+    def get_min(self, mes_type):
+        """ Retrieves the smallest measurements of either CO2 or TVOC from the database 
+            mes_type is either "CO2" or "TVOC". 
+            If not the function returns a print statement to the programmer"""
+        if mes_type in('CO2', 'TVOC'):
+            c = self.db_conn.cursor()
+            c.execute(f"SELECT MIN({mes_type}) FROM {self.db_table_name}")
+            return c.fetchall()
+        print(f"Type must be 'CO2' or 'TVOC'. Not {mes_type}")
+
+    def get_max(self, mes_type):
+        """ Retrieves the largest measurements of either CO2 or TVOC from the database 
+            mes_type is either "CO2" or "TVOC". 
+            If not the function returns a print statement to the programmer"""
+        if mes_type in('CO2', 'TVOC'):
+            c = self.db_conn.cursor()
+            c.execute(f"SELECT MAX({mes_type}) FROM {self.db_table_name}")
+            return c.fetchall()
+        print(f"Type must be 'CO2' or 'TVOC'. Not {mes_type}")
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+""" Running a webserver to show measurements on air quality obtained by sensors on a m5Stack"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
