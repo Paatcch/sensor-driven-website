f2ae054e5aa18fc4afc4bd83d72fb6b0407cc41f
Robin Højland Hansen <202409877@post.au.dk>
2025-03-06 13:05:17 +0100

    updated file structure, plus requirements and app routes

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    """ Home page (index) of site"""
+    return render_template("home.html")
+
+@app.route("/measurements")
+def measurements():
+    """ Measurements page of site """
+    return render_template("measurements.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
