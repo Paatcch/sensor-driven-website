398a06fd652dc293ec22a56778f647a1379fb8d0
Robin Højland Hansen <202409877@post.au.dk>
2025-03-14 07:00:26 +0100

    css

--- static/js/home.js
+++ static/js/home.js
@@ -36,20 +36,20 @@ function getMeasurements(){
             }else{
                 no_data_message.setAttribute('hidden', '');
                 outer_container_for_measurements.removeAttribute('hidden');
-                latCO2.textContent = `Latest value: ${response.latCO2[0]}`;
-                latCO2_time.textContent = `Time: ${to_local_time(response.latCO2[1])}`;
-                latTVOC.textContent = `Latest value: ${response.latTVOC[0]}`;
-                latTVOC_time.textContent = `Time: ${to_local_time(response.latTVOC[1])}`;
+                latCO2.textContent = response.latCO2[0];
+                latCO2_time.textContent = to_local_time(response.latCO2[1]);
+                latTVOC.textContent = response.latTVOC[0];
+                latTVOC_time.textContent = to_local_time(response.latTVOC[1]);
         
-                maxCO2.textContent = `Maximum value: ${response.maxCO2[0]}`;
-                maxCO2_time.textContent = `Time: ${to_local_time(response.maxCO2[1])}`;
-                maxTVOC.textContent = `Maximum value: ${response.maxTVOC[0]}`;
-                maxTVOC_time.textContent = `Time: ${to_local_time(response.maxTVOC[1])}`;
+                maxCO2.textContent = response.maxCO2[0];
+                maxCO2_time.textContent = to_local_time(response.maxCO2[1]);
+                maxTVOC.textContent = response.maxTVOC[0];
+                maxTVOC_time.textContent = to_local_time(response.maxTVOC[1]);
                 
-                minCO2.textContent = `Minimum value: ${response.minCO2[0]}`;
-                minCO2_time.textContent = `Time: ${to_local_time(response.minCO2[1])}`;
-                minTVOC.textContent = `Minimum value: ${response.minTVOC[0]}`;
-                minTVOC_time.textContent = `Time: ${to_local_time(response.minTVOC[1])}`;
+                minCO2.textContent = response.minCO2[0];
+                minCO2_time.textContent = to_local_time(response.minCO2[1]);
+                minTVOC.textContent = response.minTVOC[0];
+                minTVOC_time.textContent = to_local_time(response.minTVOC[1]);
             }
         }else{
             console.error(`ERROR: ${request.status}`);
